0b22ec36097c26b6ee50c7ba00db9853a934c0fd
Guillermo Marco <gmarcoremon@outlook.com>
2020-12-19 12:13:29 +0100

    Nuevas funcionalidades en la interfaz
    Se han añadido funcionales básicas:
    Barra de desplzamiento en la caja de análisis
    Menu contextual de copiar-pegar
    Corrección de bug de color de los versos en Windows
    Opción en menú de análisis de poema

--- jumper_app.py
+++ jumper_app.py
@@ -1,7 +1,8 @@
-from tkinter import *
 from tkinter.scrolledtext import ScrolledText
 from tkinter import filedialog
-from tkinter.ttk import Treeview
+from tkinter import ttk
+from tkinter import *
+import threading
 
 import jumper
 import os
@@ -49,7 +50,11 @@ class VentanaContador:
         group2.columnconfigure(0, weight=1)
 
         # Create the textbox
-        self.txt_destino = Treeview(group2, height=25, columns=self.cabecera, show='headings')
+        self.txt_destino = ttk.Treeview(group2, height=25, columns=self.cabecera, show='headings')
+        vsb = ttk.Scrollbar(group2, orient="vertical", command=self.txt_destino.yview)
+        vsb.configure(command=self.txt_destino.yview)
+        self.txt_destino.configure(yscrollcommand=vsb.set)
+        
         # set column headings
         tamanios = (50, 400, 400, 80, 180, 180, 350, 100)
         for i,col in enumerate(self.cabecera):
@@ -58,7 +63,9 @@ class VentanaContador:
         self.txt_destino.tag_configure('green', foreground='green')
         self.txt_destino.tag_configure('red', foreground='red')
         self.txt_destino.tag_configure('black', foreground='black')
-
+        
+        
+        vsb.grid(row=1, column=3, sticky=N+S)
         self.txt_destino.grid(row=1, column=0, sticky=E + W + N + S)
         self.root.columnconfigure(0, weight=1)
         self.root.rowconfigure(1, weight=1)
@@ -77,21 +84,72 @@ class VentanaContador:
         helpmenu.add_command(label="Cómo se usa", command=self.popupmsgHelp)
         helpmenu.add_separator()
         helpmenu.add_command(label="Acerca de...", command=self.popupmsgAbout)
+        
+        analisismenu = Menu(menubar, tearoff=0)
+        analisismenu.add_command(label="Analizar poema", command=self.put_text_in_txt_destino_menu)
+
 
         menubar.add_cascade(label="Archivo", menu=filemenu)
+        menubar.add_cascade(label="Análisis", menu=analisismenu)
         menubar.add_cascade(label="Ayuda", menu=helpmenu)
-        self.root.bind_all('<KeyPress>', self.put_text_in_txt_destino)
-
+        
+        self.txt_origen.bind('<Button-3>',self.rClicker, add='')
+        self.txt_origen.bind('<KeyPress>', self.put_text_in_txt_destino_on_key)
+        self.txt_origen.focus()
+        
         self.statusbar = Label(self.root, text="", bd=1, relief=SUNKEN, anchor='e', justify=LEFT)
-        self.statusbar.grid(row=3, column=1, columnspan=1)
+        self.statusbar.grid(row=3, column=0, columnspan=2)
+    
+    # https://stackoverflow.com/questions/4266566/stardand-context-menu-in-python-tkinter-text-widget-when-mouse-right-button-is-p
+    def rClicker(self, e):
 
+
+        try:
+            def rClick_Copy(e, apnd=0):
+                e.widget.event_generate('<Control-c>')
+
+            def rClick_Cut(e):
+                e.widget.event_generate('<Control-x>')
+
+            def rClick_Paste(e):
+                e.widget.event_generate('<Control-v>')
+
+            e.widget.focus()
+
+            nclst=[
+                   (' Cortar', lambda e=e: rClick_Cut(e)),
+                   (' Copiar', lambda e=e: rClick_Copy(e)),
+                   (' Pegar', lambda e=e: rClick_Paste(e)),
+                   ]
+
+            rmenu = Menu(None, tearoff=0, takefocus=0)
+
+            for (txt, cmd) in nclst:
+                rmenu.add_command(label=txt, command=cmd)
+
+            rmenu.tk_popup(e.x_root+40, e.y_root+10,entry="0")
+
+        except TclError:
+            print (' - rClick menu, something wrong')
+            pass
+
+        return "break"
+    
     def show(self,x, colores):
         self.txt_destino.delete(*self.txt_destino.get_children())
 
         for i, (v, v_etiquetado, v_silabas, v_acentos, v_extra, v_tipo, v_coin) in enumerate(x):
-            self.txt_destino.insert("", "end", values=(i+1, v, v_etiquetado, v_silabas, str(v_acentos), str(v_extra), v_tipo, v_coin),tags=colores[i])
+            self.txt_destino.insert("", "end", values=(i+1, v, v_etiquetado, v_silabas, str(v_acentos), str(v_extra), v_tipo, v_coin),tags=(colores[i],))
 
-    def put_text_in_txt_destino(self, event):
+    def put_text_in_txt_destino_on_key(self,event):
+        t = threading.Thread(target=self.put_text_in_txt_destino(), args=(self,))
+        del(t)
+
+    def put_text_in_txt_destino_menu(self):
+        t = threading.Thread(target=self.put_text_in_txt_destino(), args=(self,))
+        del(t)
+
+    def put_text_in_txt_destino(self):
         fetched_content = self.txt_origen.get('1.0', 'end-1c')
 
         try:
@@ -113,10 +171,10 @@ class VentanaContador:
         precision = 0
         colores = []
 
-        self.html_table = '<table width="100%" style="margin: 0px;font-size: 14pt;font-family: Courier;">'
+        self.html_table = '<table width="100%" style="margin: 0px;font-size: 11pt;font-family: Courier;">'
         self.html_table += '<tr>'
         for item in self.cabecera:
-            self.html_table += '<th style="border-bottom-style: dashed;">' + item + '</th>'
+            self.html_table += '<th style="text-align: left;">' + item + '</th>'
         self.html_table += '</tr>'
 
         for v in x:
@@ -146,7 +204,7 @@ class VentanaContador:
 
         #imprimimos regularidad y precision
 
-        resumen_precision = f"Precision de los acentos:  {calidad:.1f}% Regularidad con los versos más frecuentes: {regularidad:.1f}%"
+        resumen_precision = f"Precisión de los acentos:  {calidad:.1f}% Regularidad con los versos más frecuentes: {regularidad:.1f}%"
         self.statusbar['text'] = resumen_precision
 
         self.show(x,colores)
@@ -175,12 +233,12 @@ class VentanaContador:
         popup.mainloop()
 
     def popupmsgAbout(self):
-        msg = 'Analizador automático de métrica por Guillermo Marco Remón.\n Las tipologías de verso empleadas se han extraído de Pou, P. J. (2020). Métrica española. Ediciones Cátedra. \n Si detecta cualquier error escriba a gmarco@lsi.uned.es'
-        title = "Acerca de... Versión 14122020"
+        msg = 'Analizador automático de métrica por Guillermo Marco Remón.\n nlp.uned.es, Research Group in NLP & IR, UNED, Madrid, Spain.\n Las tipologías de verso empleadas se han extraído de Pou, P. J. (2020). Métrica española. Ediciones Cátedra. \n Si detecta cualquier error escriba a gmarco@lsi.uned.es'
+        title = "Acerca de... Versión 21122020"
         self.popupgen(title, msg)
 
     def popupmsgHelp(self):
-        msg = 'Pegue o escriba el poema en la sección "Poema",\nel análisis métrico se hará automáticamente. \nLa tendencia versal se calculará también automáticamente.\n Si lo desea, puede establecerla manualmente en la caja de texto.'
+        msg = 'Pegue o escriba el poema en la sección "Poema",\nel análisis métrico se hará automáticamente. \nLa tendencia versal se calculará también automáticamente.\n Si lo desea, puede establecerla manualmente separada por comas en la caja de texto: ej.: 14, 11, 7'
         title = 'Modo de empleo'
         self.popupgen(title, msg)
 
@@ -191,7 +249,7 @@ class VentanaContador:
         home = os.path.expanduser('~')
         path = filedialog.asksaveasfilename(initialdir=home, title="Guardar como",
                                             filetypes=(("html", "*.html"), ("All files", "*.*")))
-        textFile = open(path, 'w')
+        textFile = open(path+'.html', 'w')
         textFile.write(self.html_table)
         textFile.close()
 
